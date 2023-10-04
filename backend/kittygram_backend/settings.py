b456847127703c26c7cdb8301d182db271a1f661
the-world-at-large <artem-abramov-2002@mail.ru>
2023-10-04 07:31:43 +0000

    13-й (11-й) спринт. Ревью.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,6 +9,8 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.77.66', '127.0.0.1', 'localhost', 'kittttygrammm.hopto.org']
