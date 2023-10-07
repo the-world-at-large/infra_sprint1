f07cb3b43a0d4dd7739e57240c7ab5f7b6bc2b8a
the-world-at-large <artem-abramov-2002@mail.ru>
2023-10-07 15:32:15 +0000

    13-й (11-й) спринт. DONE.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
-DEBUG = (os.getenv('DEBUG', 'false').lower() != 'false')
+DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
